e853626d06780275bcea70cb7d454169a795e8e7
GregOwen <greg@sigmacomputing.com>
2021-09-14 15:03:26 -0700

    fix test

--- datatype_test.go
+++ datatype_test.go
@@ -16,16 +16,19 @@ type tcDataTypeMode struct {
 
 func TestDataTypeMode(t *testing.T) {
 	var testcases = []tcDataTypeMode{
+		{tp: DataTypeFixed, tmode: fixedType, err: nil},
+		{tp: DataTypeReal, tmode: realType, err: nil},
+		{tp: DataTypeText, tmode: textType, err: nil},
+		{tp: DataTypeDate, tmode: dateType, err: nil},
+		{tp: DataTypeVariant, tmode: variantType, err: nil},
 		{tp: DataTypeTimestampLtz, tmode: timestampLtzType, err: nil},
 		{tp: DataTypeTimestampNtz, tmode: timestampNtzType, err: nil},
 		{tp: DataTypeTimestampTz, tmode: timestampTzType, err: nil},
-		{tp: DataTypeDate, tmode: dateType, err: nil},
-		{tp: DataTypeTime, tmode: timeType, err: nil},
+		{tp: DataTypeObject, tmode: objectType, err: nil},
+		{tp: DataTypeArray, tmode: arrayType, err: nil},
 		{tp: DataTypeBinary, tmode: binaryType, err: nil},
-		{tp: DataTypeFixed, tmode: fixedType,
-			err: fmt.Errorf(errMsgInvalidByteArray, DataTypeFixed)},
-		{tp: DataTypeReal, tmode: realType,
-			err: fmt.Errorf(errMsgInvalidByteArray, DataTypeFixed)},
+		{tp: DataTypeTime, tmode: timeType, err: nil},
+		{tp: DataTypeBoolean, tmode: booleanType, err: nil},
 		{tp: 123, tmode: nullType,
 			err: fmt.Errorf(errMsgInvalidByteArray, 123)},
 	}
