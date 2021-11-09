0da187140856057b72b40fcd0f64c1c6cf273104
Agam Brahma <agam@sigmacomputing.com>
2021-11-09 14:17:05 -0800

    nit: cleaned up comment that isn't true

--- env_flags.go
+++ env_flags.go
@@ -8,7 +8,6 @@ import (
 
 // whether and how much to log, for a response-body.
 // 0 => don't log anything
-// -1 => no limit to logging
 // a valid positive number => log response body bytes up to that number
 const responseBodySampleSize = "GOSNOWFLAKE_RESPONSE_SAMPLE_SIZE"
 
