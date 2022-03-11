389fdbdc63dcccf1cc7209af93a142d546db124e
Mihai Claudiu Toader <mihai@sigmacomputing.com>
2022-03-11 14:47:50 -0800

    [Fix] SIG-18794: Fix getAsync() to not panic on context exceeded + test. (#56)

--- async_test.go
+++ async_test.go
@@ -1,12 +1,12 @@
-// Copyright (c) 2021-2023 Snowflake Computing Inc. All rights reserved.
+// Copyright (c) 2021-2022 Snowflake Computing Inc. All rights reserved.
 
 package gosnowflake
 
 import (
 	"context"
-	"database/sql"
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestAsyncMode(t *testing.T) {
@@ -19,7 +19,7 @@ func TestAsyncMode(t *testing.T) {
 	var idx int
 	var v string
 
-	runDBTest(t, func(dbt *DBTest) {
+	runTests(t, dsn, func(dbt *DBTest) {
 		rows := dbt.mustQueryContext(ctx, fmt.Sprintf(selectRandomGenerator, numrows))
 		defer rows.Close()
 
@@ -48,39 +48,12 @@ func TestAsyncMode(t *testing.T) {
 	})
 }
 
-func TestAsyncModeMultiStatement(t *testing.T) {
-	withMultiStmtCtx, _ := WithMultiStatement(context.Background(), 6)
-	ctx := WithAsyncMode(withMultiStmtCtx)
-	multiStmtQuery := "begin;\n" +
-		"delete from test_multi_statement_async;\n" +
-		"insert into test_multi_statement_async values (1, 'a'), (2, 'b');\n" +
-		"select 1;\n" +
-		"select 2;\n" +
-		"rollback;"
-
-	runDBTest(t, func(dbt *DBTest) {
-		dbt.mustExec("drop table if exists test_multi_statement_async")
-		dbt.mustExec(`create or replace table test_multi_statement_async(
-			c1 number, c2 string) as select 10, 'z'`)
-		defer dbt.mustExec("drop table if exists test_multi_statement_async")
-
-		res := dbt.mustExecContext(ctx, multiStmtQuery)
-		count, err := res.RowsAffected()
-		if err != nil {
-			t.Fatalf("res.RowsAffected() returned error: %v", err)
-		}
-		if count != 3 {
-			t.Fatalf("expected 3 affected rows, got %d", count)
-		}
-	})
-}
-
 func TestAsyncModeCancel(t *testing.T) {
 	withCancelCtx, cancel := context.WithCancel(context.Background())
 	ctx := WithAsyncMode(withCancelCtx)
 	numrows := 100000
 
-	runDBTest(t, func(dbt *DBTest) {
+	runTests(t, dsn, func(dbt *DBTest) {
 		dbt.mustQueryContext(ctx, fmt.Sprintf(selectRandomGenerator, numrows))
 		cancel()
 	})
@@ -88,7 +61,7 @@ func TestAsyncModeCancel(t *testing.T) {
 
 func TestAsyncQueryFail(t *testing.T) {
 	ctx := WithAsyncMode(context.Background())
-	runDBTest(t, func(dbt *DBTest) {
+	runTests(t, dsn, func(dbt *DBTest) {
 		rows := dbt.mustQueryContext(ctx, "selectt 1")
 		defer rows.Close()
 
@@ -109,22 +82,15 @@ func TestMultipleAsyncQueries(t *testing.T) {
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	db := openDB(t)
-
-	runDBTest(t, func(dbt *DBTest) {
-		rows1, err := db.QueryContext(ctx, fmt.Sprintf("select distinct '%v' from table (generator(timelimit=>%v))", s1, 30))
-		if err != nil {
-			t.Fatalf("can't read rows1: %v", err)
-		}
+	runTests(t, dsn, func(dbt *DBTest) {
+		rows1 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%v' from table (generator(timelimit=>%v))", s1, 30))
 		defer rows1.Close()
-		rows2, err := db.QueryContext(ctx, fmt.Sprintf("select distinct '%v' from table (generator(timelimit=>%v))", s2, 10))
-		if err != nil {
-			t.Fatalf("can't read rows2: %v", err)
-		}
+		rows2 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%v' from table (generator(timelimit=>%v))", s2, 10))
 		defer rows2.Close()
 
 		go retrieveRows(rows1, ch1)
 		go retrieveRows(rows2, ch2)
+
 		select {
 		case res := <-ch1:
 			t.Fatalf("value %v should not have been called earlier.", res)
@@ -136,7 +102,41 @@ func TestMultipleAsyncQueries(t *testing.T) {
 	})
 }
 
-func retrieveRows(rows *sql.Rows, ch chan string) {
+func TestMultipleAsyncSuccessAndFailedQueries(t *testing.T) {
+	ctx := WithAsyncMode(context.Background())
+	ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+	defer cancel()
+
+	s1 := "foo"
+	s2 := "bar"
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	runTests(t, dsn, func(dbt *DBTest) {
+		rows1 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>3))", s1))
+		defer rows1.Close()
+
+		rows2 := dbt.mustQueryContext(ctx, fmt.Sprintf("select distinct '%s' from table (generator(timelimit=>7))", s2))
+		defer rows2.Close()
+
+		go retrieveRows(rows1, ch1)
+		go retrieveRows(rows2, ch2)
+
+		res1 := <-ch1
+		if res1 != s1 {
+			t.Fatalf("query failed. expected: %v, got: %v", s1, res1)
+		}
+
+		// wait until rows2 is done
+		<-ch2
+		driverErr, ok := rows2.Err().(*SnowflakeError)
+		if !ok || driverErr == nil || driverErr.Number != ErrAsync {
+			t.Fatalf("Snowflake ErrAsync expected. got: %T, %v", rows2.Err(), rows2.Err())
+		}
+	})
+}
+
+func retrieveRows(rows *RowsExtended, ch chan string) {
 	var s string
 	for rows.Next() {
 		if err := rows.Scan(&s); err != nil {
@@ -147,39 +147,4 @@ func retrieveRows(rows *sql.Rows, ch chan string) {
 	}
 	ch <- s
 	close(ch)
-}
-
-func TestLongRunningAsyncQuery(t *testing.T) {
-	conn := openConn(t)
-	defer conn.Close()
-
-	ctx, _ := WithMultiStatement(context.Background(), 0)
-	query := "CALL SYSTEM$WAIT(50, 'SECONDS');use snowflake_sample_data"
-
-	rows, err := conn.QueryContext(WithAsyncMode(ctx), query)
-	if err != nil {
-		t.Fatalf("failed to run a query. %v, err: %v", query, err)
-	}
-	defer rows.Close()
-	var v string
-	i := 0
-	for {
-		for rows.Next() {
-			err := rows.Scan(&v)
-			if err != nil {
-				t.Fatalf("failed to get result. err: %v", err)
-			}
-			if v == "" {
-				t.Fatal("should have returned a result")
-			}
-			results := []string{"waited 50 seconds", "Statement executed successfully."}
-			if v != results[i] {
-				t.Fatalf("unexpected result returned. expected: %v, but got: %v", results[i], v)
-			}
-			i++
-		}
-		if !rows.NextResultSet() {
-			break
-		}
-	}
 }
