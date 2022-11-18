1273bc6aa03ad5c717aa515c370f1550c0abe4b3
Madison Chamberlain <46542378+madisonchamberlain@users.noreply.github.com>
2022-11-17 20:06:19 -0800

    address comments about return types and desc

--- submit_async.go
+++ submit_async.go
@@ -10,30 +10,43 @@ import (
 	"strconv"
 )
 
-type isComplete bool
-type queryID string
+type SubmitAsyncResponse struct {
+	QueryId string
+	Complete bool
+}
 
+// SubmitAsync returns a queryId which can be used to check the status of a query (or later, get query results when a 
+// query is completed) and returns whether the query is complete so we can avoid coming back to wait for it to complete
 func (sc *snowflakeConn) SubmitAsync(
 	ctx context.Context,
 	query string,
 	args []driver.NamedValue,
-) (queryID, isComplete, error) {
+) (SubmitAsyncResponse, error) {
 	ctx = WithAsyncMode(WithAsyncModeNoFetch(ctx))
 	qid, err := getResumeQueryID(ctx)
 	if err != nil {
-		return "", false, err
+		return SubmitAsyncResponse {
+			QueryId: "",
+			Complete: false,
+		}, err
 	}
 
 	// if query already submitted, return the query id
 	if qid != "" {
-		return queryID(qid), false, nil
+		return SubmitAsyncResponse {
+			QueryId: qid,
+			Complete: sc.checkIfComplete(ctx, qid),
+		}, err
 	}
 
 	// if the query is not submitted, submit it
 	logger.WithContext(ctx).Infof("Query: %#v, %v", query, args)
 
 	if sc.rest == nil {
-		return "", false, driver.ErrBadConn
+		return SubmitAsyncResponse {
+			QueryId: "",
+			Complete: false,
+		}, err
 	}
 
 	ctx = setResultType(ctx, queryResultType)
@@ -45,26 +58,45 @@ func (sc *snowflakeConn) SubmitAsync(
 		if data != nil {
 			code, err := strconv.Atoi(data.Code)
 			if err != nil {
-				return "", true, err
+				return SubmitAsyncResponse {
+					QueryId: qid,
+					Complete: true,
+				}, err
 			}
-			return "", true, (&SnowflakeError{
+			return SubmitAsyncResponse {
+				QueryId: qid,
+				Complete: true,
+			}, (&SnowflakeError{
 				Number:   code,
 				SQLState: data.Data.SQLState,
 				Message:  err.Error(),
 				QueryID:  data.Data.QueryID,
 			}).exceptionTelemetry(sc)
 		}
-		return "", true, err
+		return SubmitAsyncResponse {
+			QueryId: qid,
+			Complete: true,
+		}, err
 	}
 
 	qid = data.Data.QueryID
-	_, err = sc.checkQueryStatus(ctx, qid)
-	if err == nil {
-		// no error means query is complete
-		return queryID(qid), true, nil
-	}
 
-	return queryID(qid), false, nil
+	// after the query is submitted, we check the status of the query. This will save us time because the entire round trip of checking 
+	// status from here only takes 100 ms, so for fast running queries this will prevent another roundtrip from multiplex, and also prevent
+	// inconsistencies that can occur for very fast running queries  
+	return SubmitAsyncResponse {
+		QueryId: qid,
+		Complete: sc.checkIfComplete(ctx, qid),
+	}, nil
+}
+
+func(sc *snowflakeConn) checkIfComplete(ctx context.Context, qid string) bool {
+	_, err := sc.checkQueryStatus(ctx, qid)
+	if err == nil || err.(*SnowflakeError).Number != ErrQueryIsRunning {
+		// no error means query is complete; error that isnt running means query complete but failed 
+		return true
+	}
+	return false 
 }
 
 // AsyncSubmitter is an interface which allows a query to be submitted
@@ -75,5 +107,5 @@ func (sc *snowflakeConn) SubmitAsync(
 // export it so that clients can access this functionality, bypassing
 // the alternative which is using the queryContext built into dbSql
 type AsyncSubmitter interface {
-	SubmitAsync(context.Context, string, []driver.NamedValue) (queryID, isComplete, error) 
+	SubmitAsync(context.Context, string, []driver.NamedValue) (SubmitAsyncResponse, error) 
 }
