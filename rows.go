0652cacb7153d8d9076c36c6857a26f12ff565b3
Yifeng-Sigma <yifeng@sigmacomputing.com>
2023-06-05 13:59:31 -0700

    get rid of url in tracing (#142)

--- rows.go
+++ rows.go
@@ -187,7 +187,9 @@ func (rows *snowflakeRows) GetChunkMetas() []ExecResponseChunk {
 	execResponseChunkExport := make([]ExecResponseChunk, len(execResponseChunkPrivate))
 	for i := 0; i < len(execResponseChunkPrivate); i++ {
 		execResponseChunkExport[i] = ExecResponseChunk{
-			execResponseChunkPrivate[i],
+			RowCount:         execResponseChunkPrivate[i].RowCount,
+			UncompressedSize: execResponseChunkPrivate[i].UncompressedSize,
+			CompressedSize:   execResponseChunkPrivate[i].CompressedSize,
 		}
 	}
 	return execResponseChunkExport
