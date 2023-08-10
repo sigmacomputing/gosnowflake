77d8b75ba873850ce7d0eb1dcd974e14138d35e4
Yifeng Wu <yifeng@sigmacomputing.com>
2023-08-09 20:05:39 -0700

    convert binary array to string array

--- converter_test.go
+++ converter_test.go
@@ -886,6 +886,17 @@ func TestArrowToRecord(t *testing.T) {
 			append:  func(b array.Builder, vs interface{}) { b.(*array.BinaryBuilder).AppendValues(vs.([][]byte), valids) },
 		},
 		{
+			logical: "non utf-8 binary",
+			sc:      arrow.NewSchema([]arrow.Field{{Type: &arrow.BinaryType{}}}, nil),
+			values: [][]byte{
+				{0x80, 0x81, 0x82},
+				{0xF0, 0xF1, 0xF2},
+			},
+			nrows:   2,
+			builder: array.NewBinaryBuilder(pool, arrow.BinaryTypes.Binary),
+			append:  func(b array.Builder, vs interface{}) { b.(*array.BinaryBuilder).AppendValues(vs.([][]byte), valids) },
+		},
+		{
 			logical: "date",
 			sc:      arrow.NewSchema([]arrow.Field{{Type: &arrow.Date32Type{}}}, nil),
 			values:  []time.Time{time.Now(), localTime},
