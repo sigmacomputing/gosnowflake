155ec9774e350e836396e040af6aeb45197c293b
Eric Bannatyne <eric@sigmacomputing.com>
2023-10-27 11:13:35 -0700

    lint, fmt
    Signed-off-by: Eric Bannatyne <eric@sigmacomputing.com>

--- auth_test.go
+++ auth_test.go
@@ -102,20 +102,6 @@ func postAuthSuccess(_ context.Context, _ *snowflakeRestful, _ *http.Client, _ *
 	}, nil
 }
 
-func postAuthSuccessWithServerVersion(_ context.Context, _ *snowflakeRestful, _ *http.Client, _ *url.Values, _ map[string]string, _ bodyCreatorType, _ time.Duration) (*authResponse, error) {
-	return &authResponse{
-		Success: true,
-		Data: authResponseMain{
-			Token:       "t",
-			MasterToken: "m",
-			SessionInfo: authResponseSessionInfo{
-				DatabaseName: "dbn",
-			},
-			ServerVersion: "123.456.7",
-		},
-	}, nil
-}
-
 func postAuthCheckSAMLResponse(_ context.Context, _ *snowflakeRestful, _ *http.Client, _ *url.Values, _ map[string]string, bodyCreator bodyCreatorType, _ time.Duration) (*authResponse, error) {
 	var ar authRequest
 	jsonBody, _ := bodyCreator()
@@ -697,33 +683,6 @@ func TestUnitAuthenticateExternalBrowser(t *testing.T) {
 	_, err = authenticate(context.TODO(), sc, []byte{}, []byte{})
 	if err == nil {
 		t.Fatal("should have failed")
-	}
-}
-
-func TestUnitLogAuthSuccessMetadata(t *testing.T) {
-	serverVersion := ""
-	RegisterAuthSuccessHook(func(_ context.Context, metadata SessionMetadata) {
-		serverVersion = metadata.ServerVersion
-	})
-	t.Cleanup(func() {
-		RegisterAuthSuccessHook(nil)
-	})
-
-	sr := &snowflakeRestful{
-		FuncPostAuth:  postAuthSuccessWithServerVersion,
-		TokenAccessor: getSimpleTokenAccessor(),
-	}
-	sc := getDefaultSnowflakeConn()
-	sc.ctx = context.TODO()
-	sc.rest = sr
-	sc.cfg.Authenticator = AuthTypeSnowflake
-
-	if err := authenticateWithConfig(sc); err != nil {
-		t.Fatalf("failed to run. err: %v", err)
-	}
-
-	if serverVersion != "123.456.7" {
-		t.Fatalf("Expected server version 123.456.7, got %v", serverVersion)
 	}
 }
 
